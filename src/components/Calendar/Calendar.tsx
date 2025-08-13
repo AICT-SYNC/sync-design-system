90c111e838d7480ce12516270840d4fa9763f164
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-08-13 19:46:39 +0900

    feat: calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,3 +1,32 @@
-export const Calendar = () => {
-  return <div>Calendar Component</div>;
+import React from "react";
+import styled, { css } from "styled-components";
+import { Calendar as CalendarIcon } from "../../assets/icons/calendar";
+
+type sizeType =
+  | "medium"
+  | "large";
+
+interface CalendarProps {
+  size?: sizeType;
+}
+
+const StyledCalendar = styled.div<CalendarProps>`
+  
+`;
+
+const Divider = styled.div`
+  width: 1px;
+  height: 24px;
+  background-color: #e0e0e0;
+  margin: 0 8px;
+`;
+
+export const Calendar:React.FC<CalendarProps> = ({size = "medium"}) => {
+  return (
+    <StyledCalendar size={size}>
+      
+      <CalendarIcon color="gray" />
+      <Divider />
+    </StyledCalendar>
+  );
 }

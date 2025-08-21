ac48928bec37ad4a1264d6bddbb6b6382ad6ac0c
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-08-21 14:58:43 +0900

    fix: fix route

--- src/assets/icons/Stories/avatar.stories.tsx
+++ src/assets/icons/Stories/avatar.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Avatar } from "./avatar";
+import { Avatar } from "../avatar";
 
 interface AvatarProps {
   size?: 'extraSmall' | 'small' | 'medium' | 'large' | 'extraLarge' | 'xxl';

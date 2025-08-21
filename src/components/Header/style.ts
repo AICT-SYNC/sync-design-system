b9c45978588e8311ff1680efbe5d81e4160e7ecd
Yongjin081002 <yg4059540@gmail.com>
2025-08-21 19:57:47 +0900

    feat: create header component base

---
+++ src/components/Header/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { lightColors } from '../../tokens/LightColors';
+import { Font } from '../../tokens/Font';
+
+
+export const HeaderContainer = styled.div`
+    width:100%;
+    height:40px;
+`

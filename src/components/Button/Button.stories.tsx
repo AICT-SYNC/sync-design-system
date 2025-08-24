c26cb5ff242432d1f57437e54efce2c5bdc64635
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-08-24 19:26:37 +0900

    fix: file rename

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button, ButtonProps } from "./Button";
+import { Button, ButtonProps } from "./bugButton";
 import { ButtonVariant } from "../../foundation/Button";
 
 const meta: Meta<ButtonProps> = {

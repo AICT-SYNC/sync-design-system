c65a74ed2d28e6551a186645c61ab41325755d6b
dlrjsgml44 <leegeh1213@gmail.com>
2025-08-24 11:23:13 +0900

    refactor: Change Header Props

--- src/components/Header/types.ts
+++ src/components/Header/types.ts
@@ -1,9 +1,8 @@
+
 export interface HeaderProps {
-  // 탭 관련
   id: string;
   title: string;
   isActive: boolean;
-  isHover: boolean
-  onTabClick: (tabId: string) => void;
-  onTabClose: (tabId: string) => void;
+  onTabClick: (id: string) => void;
+  onTabClose?: (id: string) => void;
 }
